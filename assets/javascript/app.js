7604a6a2bae962373ef322b472d52128e45424b7
enguyen93 <enguyenusaf93@gmail.com>
2018-09-16 15:13:54 -0500

    got the questions and answers to update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,66 +1,60 @@
 var questionHolder = $(".questionHolder");
-var answerLoc1 = $("#answerLoc1");
-var answerLoc2 = $("#answerLoc2");
-var answerLoc3 = $("#answerLoc3");
-var answerLoc4 = $("#answerLoc4");
+var answerHolder = $(".answerHolder");
+
+var currentQ = 0;
+var currentA = 0;
+
+var questions = [{
+    question: "what halo"
+}, {
+    question: "what cod"
+}, {
+    question: "what pokemon"
+}];
 
 
-var obj = {
-    questions: [{
-        question: "what halo",
-        answer1: "1",
-        answer2: "2",
-        answer3: "3",
-        answer4: "4"
+
+var answers = {
+    array: [{
+        answers: ["1", "2", "3", "4"]
     }, {
-        question: "what cod",
-        answer1: "1",
-        answer2: "2",
-        answer3: "3",
-        answer4: "4"
+        answers: ["waw", "cod 4", "mw2", "mw3"]
     }, {
-        question: "what pokemon",
-        answer1: "yellow",
-        answer2: "blue",
-        answer3: "red",
-        answer4: "sapphire"
-    }]
+        answers: ["yellow", "sapphire", "ruby", "blue"]
+    }
+    ]
 };
 
 function startTimer(duration, timeRemaining) {
     var timer = duration, seconds;
-    setInterval(function(){
+    setInterval(function () {
         seconds = parseInt(timer % 60, 10);
 
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
         timeRemaining.text("Time Remaining: " + seconds);
 
-        if (--timer < 0) {
+        if (--timer <= -1) {
             timer = duration;
+            currentQ++;
+            currentA++;
+            askQuestion();
+
         }
     }, 1000);
 }
 
 jQuery(function ($) {
-    var oneMinute = 60 * 1;
-        timeRemaining = $("#timeRemaining");
-    startTimer(oneMinute,timeRemaining); 
+    var oneMinute = 10 * 1;
+    timeRemaining = $("#timeRemaining");
+    startTimer(oneMinute, timeRemaining);
 });
 
-function question() {
-    for(var i=0; i<obj.questions[0]; i++) {
-        console.log(i);
+function askQuestion() {
+    questionHolder.html(questions[currentQ].question);
+    for (i = 0; i < answers.array.length; i++) {
+        answerHolder.html(answers.array[currentA].answers.join("   "));
     }
 }
-question();
+askQuestion();
 
-
-// function question() {
-//     questionHolder.text(obj.questions[0].question);
-//     answerLoc1.text(obj.questions[0].answer1);
-//     answerLoc2.text(obj.questions[0].answer2);
-//     answerLoc3.text(obj.questions[0].answer3);
-//     answerLoc4.text(obj.questions[0].answer4);
-// }
-// question();
